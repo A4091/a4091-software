cbce4daf06c75eadf12e41f0c109ad860e2df61a
Stefan Reinauer <stefan.reinauer@coreboot.org>
2022-08-27 22:22:24 -0700

    Add scsi inquiries to scsimsg.c

--- scsimsg.c
+++ scsimsg.c
@@ -33,6 +33,62 @@ uint16_t sdcmd_read_blocks(struct IOStdReq *ioreq, uint8_t *data, uint32_t block
     return 0;
 }
 
+int do_scsi_inquiry(struct IOExtTD *tio, uint lun, scsi_inquiry_data_t **inq)
+{
+    int rc;
+    scsi_inquiry_data_t *res;
+    scsi_generic_t cmd;
+    struct SCSICmd scmd;
+
+#define SCSIPI_INQUIRY_LENGTH_SCSI2     36
+    res = (scsi_inquiry_data_t *) AllocMem(sizeof (*res), MEMF_PUBLIC);
+    if (res == NULL) {
+        printf("AllocMem ");
+        *inq = NULL;
+        return (1);
+    }
+
+    memset(&cmd, 0, sizeof (cmd));
+    cmd.opcode = INQUIRY;
+    cmd.bytes[0] = lun << 5;
+    cmd.bytes[1] = 0;  // Page code
+    cmd.bytes[2] = 0;
+    cmd.bytes[3] = sizeof (scsi_inquiry_data_t);
+    cmd.bytes[4] = 0;  // Control
+
+    memset(&scmd, 0, sizeof (scmd));
+    scmd.scsi_Data = (UWORD *) res;
+    scmd.scsi_Length = sizeof (*res);
+    // scmd.scsi_Actual = 0;
+    scmd.scsi_Command = (UBYTE *) &cmd;
+    scmd.scsi_CmdLength = 6;
+    // scmd.scsi_CmdActual = 0;
+    scmd.scsi_Flags = SCSIF_READ | SCSIF_AUTOSENSE;
+    // scmd.scsi_Status = 0;
+    scmd.scsi_SenseData = sense_data;
+    scmd.scsi_SenseLength = sizeof (sense_data);
+    // scmd.scsi_SenseActual = 0;
+
+    tio->iotd_Req.io_Command = HD_SCSICMD;
+    tio->iotd_Req.io_Length  = sizeof (scmd);
+    tio->iotd_Req.io_Data    = &scmd;
+
+    tio->iotd_Req.io_Message.mn_ReplyPort=CreateMsgPort();
+
+    PutMsg(myPort, &tio->iotd_Req.io_Message);
+    WaitPort(tio->iotd_Req.io_Message.mn_ReplyPort);
+    DeleteMsgPort(tio->iotd_Req.io_Message.mn_ReplyPort);
+
+    rc = 0; // FIXME how do I get rc?
+
+    if (rc != 0) {
+        FreeMem(res, sizeof (*res));
+        res = NULL;
+    }
+    *inq = res;
+    return (rc);
+}
+
 int safe_open(struct IOStdReq *ioreq, uint scsi_unit)
 {
     ioreq->io_Message.mn_Node.ln_Type = NT_REPLYMSG;
--- scsimsg.h
+++ scsimsg.h
@@ -1,7 +1,48 @@
 #ifndef SCSIMSG_H
 #define SCSIMSG_H 1
 
+#include <devices/trackdisk.h>
+
+#define INQUIRY                 0x12
+typedef struct scsi_inquiry_data {
+    uint8_t device;
+#define SID_TYPE                0x1f    /* device type mask */
+#define SID_QUAL                0xe0    /* device qualifier mask */
+#define SID_QUAL_LU_NOTPRESENT  0x20    /* logical unit not present */
+    uint8_t dev_qual2;
+    uint8_t version;
+    uint8_t response_format;
+    uint8_t additional_length;
+    uint8_t flags1;
+
+    uint8_t flags2;
+#define SID_REMOVABLE           0x80
+    uint8_t flags3;
+#define SID_SftRe       0x01
+#define SID_CmdQue      0x02
+#define SID_Linked      0x08
+#define SID_Sync        0x10
+#define SID_WBus16      0x20
+#define SID_WBus32      0x40
+#define SID_RelAdr      0x80
+
+#define SID_REMOVABLE           0x80
+    uint8_t vendor[8];
+    uint8_t product[16];
+    uint8_t revision[4];
+    uint8_t vendor_specific[20];
+    uint8_t flags4;
+    uint8_t reserved;
+    uint8_t version_descriptor[8][2];
+} __packed scsi_inquiry_data_t;  // 74 bytes
+
+typedef struct scsi_generic {
+    uint8_t opcode;
+    uint8_t bytes[15];
+} __packed scsi_generic_t;
+
 uint16_t sdcmd_read_blocks(struct IOStdReq * ioreq, uint8_t* data, uint32_t block, uint32_t len);
+int do_scsi_inquiry(struct IOExtTD *tio, uint lun, scsi_inquiry_data_t **inq);
 
 int safe_open(struct IOStdReq *ioreq, uint scsi_unit);
 void safe_close(struct IOStdReq *ioreq);
