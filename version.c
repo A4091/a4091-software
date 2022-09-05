4b5df222fbb712ea0fb3fa6ff93d5a815ccaa5c6
Stefan Reinauer <stefan.reinauer@coreboot.org>
2022-09-05 22:17:43 +0800

    version.c: version_str only needed with serial output

--- version.c
+++ version.c
@@ -1,6 +1,9 @@
 #include "version.h"
+#include "port.h"
 
 #define STR(s) #s      // Turn s into a string literal without macro expansion
 #define XSTR(s) STR(s) // Turn s into a string literal after macro expansion
+#ifdef USE_SERIAL_OUTPUT
 const char * const version_str =
     XSTR(DEVICE_VERSION)"."XSTR(DEVICE_REVISION)" built "BUILD_DATE" "BUILD_TIME;
+#endif
