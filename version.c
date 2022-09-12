c4d852370dd4bbc4f4919d1bd293ce63b6ad929e
Stefan Reinauer <stefan.reinauer@coreboot.org>
2022-09-12 22:18:57 +0800

    Compilation fix for -DDEBUG_DEVICE
    version.c is only pulled in if -DDEBUG_DEVICE is enabled. In that case
    USE_SERIAL_OUTPUT is undefined in version.c however. Make sure version_str
    is available in the right situations.

--- version.c
+++ version.c
@@ -3,7 +3,7 @@
 
 #define STR(s) #s      // Turn s into a string literal without macro expansion
 #define XSTR(s) STR(s) // Turn s into a string literal after macro expansion
-#ifdef USE_SERIAL_OUTPUT
+#ifdef DEBUG_DEVICE
 const char * const version_str =
     XSTR(DEVICE_VERSION)"."XSTR(DEVICE_REVISION)" built "BUILD_DATE" "BUILD_TIME;
 #endif
