07e1b2862e309c99bc3d5e69eb7c3a6de40b842d
Stefan Reinauer <stefan.reinauer@coreboot.org>
2022-09-28 10:38:01 +0800

    Move Amiga HW registers out of bootmenu.c

--- bootmenu.c
+++ bootmenu.c
@@ -34,6 +34,7 @@
 #include "attach.h"
 #include "scsimsg.h"
 #include "battmem.h"
+#include "amigahw.h"
 
 extern struct ExecBase *SysBase;
 struct GfxBase *GfxBase;
@@ -61,28 +62,6 @@ static struct NewGadget *NewGadget;
 
 #define ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
 #define WIDTH  640
-
-/* Amiga hardware registers */
-#define REG_CIAAPRA      0xBFE001  // CIA-A Port A input bits [Read/Write]
-#define REG_CIAAPRA_PA0  (1<<0)    // CIA-A OVL ROM/RAM Overlay Control
-#define REG_CIAAPRA_PA1  (1<<1)    // CIA-A LED / Audio Filter (0 = bright)
-#define REG_CIAAPRA_PA2  (1<<2)    // CIA-A CHNG Disk Change
-#define REG_CIAAPRA_PA3  (1<<3)    // CIA-A WPRO Write Protect
-#define REG_CIAAPRA_PA4  (1<<4)    // CIA-A TK0 Disk Track 0
-#define REG_CIAAPRA_PA5  (1<<5)    // CIA-A RDY Disk Ready
-#define REG_CIAAPRA_PA6  (1<<6)    // CIA-A Game Port 0 fire button (pin 6)
-#define REG_CIAAPRA_PA7  (1<<7)    // CIA-A Game Port 1 fire button (pin 6)
-
-#define REG_POTGOR       0xDff016  // Paula proportional pin values [Read]
-#define REG_POTGO        0xDff034  // Paula proportional pin config [Write]
-#define REG_POTGOR_DATLX (1<<8)    // Paula Pin 32 P0X Data
-#define REG_POTGOR_OUTLX (1<<9)    // Paula Pin 32 P0X Output enable
-#define REG_POTGOR_DATLY (1<<10)   // Paula Pin 33 P0Y Data
-#define REG_POTGOR_OUTLY (1<<11)   // Paula Pin 33 P0Y Output enable
-#define REG_POTGOR_DATRX (1<<12)   // Paula Pin 35 P1X Data
-#define REG_POTGOR_OUTRX (1<<13)   // Paula Pin 35 P1X Output enable
-#define REG_POTGOR_DATRY (1<<14)   // Paula Pin 36 P1Y Data
-#define REG_POTGOR_OUTRY (1<<15)   // Paula Pin 36 P1Y Output enable
 
 static const struct TextAttr font_attr =
 {
