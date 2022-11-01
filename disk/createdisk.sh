4b4a6917768317df415e626d8eb1862553d7e133
Stefan Reinauer <stefan.reinauer@coreboot.org>
2022-10-31 22:35:31 -0700

    Polish AmigaGuide file

--- disk/createdisk.sh
+++ disk/createdisk.sh
@@ -2,14 +2,17 @@
 
 DISK=A4091_4223.adf
 
-wget http://aminet.net/disk/misc/bffs16_src.lha
-lha x bffs16_src.lha
-git clone https://github.com/cdhooper/amiga_devtest.git
+echo "Downloading..."
+wget -q http://aminet.net/disk/misc/bffs16_src.lha
+lha xq bffs16_src.lha
+git clone -q https://github.com/cdhooper/amiga_devtest.git
+
+echo "Building..."
 cd amiga_devtest
-make
+make -s
 cd ..
 
-
+echo "Creating disk..."
 xdftool $DISK format "Amiga4091"
 xdftool $DISK makedir Tools
 xdftool $DISK write bffs_1.6/bin/rdb Tools/rdb
@@ -20,6 +23,9 @@ xdftool $DISK write A4091.guide
 xdftool $DISK write A4091.guide.info
 xdftool $DISK write Disk.info
 
+echo "Cleaning up..."
 rm -rf bffs_1.6 bffs16_src.lha
 rm -rf amiga_devtest
 
+echo "Done:"
+xdftool $DISK list
