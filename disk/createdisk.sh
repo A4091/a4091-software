83d3b00f5b3a6b3c8f9228986042784ff55764f2
Stefan Reinauer <stefan.reinauer@coreboot.org>
2023-05-08 08:50:46 +0200

    disk: Produce A4091 adf from submodules
    Signed-off-by: Stefan Reinauer <stefan.reinauer@coreboot.org>

--- disk/createdisk.sh
+++ disk/createdisk.sh
@@ -2,30 +2,34 @@
 
 VER=$(awk '/#define DEVICE_/{if (V != "") print V""$NF; else V=$NF}' ../version.h)
 DISK=A4091_$VER.adf
+THIRDPARTY=../3rdparty
 
-echo "Downloading..."
-wget -q http://aminet.net/disk/misc/bffs16_src.lha
-lha xq bffs16_src.lha
-git clone -q https://github.com/cdhooper/amiga_devtest.git
+echo "Looking for submodules..."
+test ! -x $THIRDPARTY/bffs/dist/bffs16_src.lha || ( echo "Initialize submodules."; exit 1 )
 
-echo "Building..."
-make -s -C amiga_devtest || exit 1
+echo "Building devtest..."
+make -s -C $THIRDPARTY/devtest || exit 1
+echo "Extracting rdb..."
+lha xiq2f $THIRDPARTY/bffs/dist/bffs16_src.lha bffs_1.6/bin/rdb
+
 
 echo "Creating disk..."
 xdftool $DISK format "Amiga4091"
 xdftool $DISK makedir Tools
-xdftool $DISK write bffs_1.6/bin/rdb Tools/rdb
+xdftool $DISK write rdb Tools/rdb
 xdftool $DISK write ../a4091 Tools/a4091
 xdftool $DISK write ../a4091d Tools/a4091d
-xdftool $DISK write amiga_devtest/devtest Tools/devtest
+xdftool $DISK write $THIRDPARTY/devtest/devtest Tools/devtest
 xdftool $DISK write A4091.guide
 xdftool $DISK write A4091.guide.info
 xdftool $DISK write Disk.info
 
 echo "Cleaning up..."
-rm -rf bffs_1.6 bffs16_src.lha
-rm -rf amiga_devtest
+make -s -C $THIRDPARTY/devtest clean
+rm rdb
 
-echo "Done:"
+echo "Done. Please verify disk contents of $DISK below:"
+echo "------------------------------------------------------------------------------------------"
 xdftool $DISK list
+echo "------------------------------------------------------------------------------------------"
 echo "Created $DISK"
