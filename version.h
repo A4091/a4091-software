70339266b356fd7f43043bd45ea030a07114d569
Stefan Reinauer <stefan.reinauer@coreboot.org>
2023-10-31 21:20:53 -0700

    Update version number after 42.26 release

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define _VERSION_H
 
 #define DEVICE_VERSION  42
-#define DEVICE_REVISION 26
+#define DEVICE_REVISION 27
 
 extern const char *const version_str;
 extern const char device_id_string[];
