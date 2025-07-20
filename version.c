d9455c3a98bf7279bdfe9bf35ffe9ae4c7d6eeb1
Stefan Reinauer <stefan.reinauer@coreboot.org>
2025-07-20 08:55:49 -0700

    Remove dead / duplicate code

--- version.c
+++ version.c
@@ -14,8 +14,6 @@
 #include "version.h"
 #include "port.h"
 
-#define STR(s) #s      // Turn s into a string literal without macro expansion
-#define XSTR(s) STR(s) // Turn s into a string literal after macro expansion
 #ifdef DEBUG_DEVICE
 const char * const version_str =
     XSTR(DEVICE_VERSION)"."XSTR(DEVICE_REVISION)" built "BUILD_DATE" "BUILD_TIME;
