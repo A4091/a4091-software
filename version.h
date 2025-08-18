9ba8ef6040afa2df18700bd2bd6f4eaabc50352d
Stefan Reinauer <stefan.reinauer@coreboot.org>
2025-08-17 18:35:48 -0700

    Increase version number

--- version.h
+++ version.h
@@ -15,7 +15,7 @@
 #define _VERSION_H
 
 #define DEVICE_VERSION  42
-#define DEVICE_REVISION 32
+#define DEVICE_REVISION 33
 
 extern const char *const version_str;
 extern const char device_id_string[];
